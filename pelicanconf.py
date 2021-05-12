3c4eea3d2ea617e5e65d8945f56a6e20ecdfe56f
Charlie Redmon <chredmon@protonmail.com>
2021-05-12 18:14:43 +0100

    check new theme

--- pelicanconf.py
+++ pelicanconf.py
@@ -11,6 +11,8 @@ TIMEZONE = 'Europe/London'
 
 DEFAULT_LANG = 'en'
 
+THEME = 'notmyidea'
+
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
