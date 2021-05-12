40730fffa2c12bb2d0fdc07f355572c0bb06ebe8
Charlie Redmon <chredmon@protonmail.com>
2021-05-12 18:43:25 +0100

    back to simple theme

--- pelicanconf.py
+++ pelicanconf.py
@@ -11,7 +11,7 @@ TIMEZONE = 'Europe/London'
 
 DEFAULT_LANG = 'en'
 
-THEME = './slrbtheme'
+# THEME = './slrbtheme'
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
