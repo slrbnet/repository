d0e0e0e88ee7f629bf45747f7c81cb8d7c46b5c9
Charlie Redmon <chredmon@protonmail.com>
2022-09-29 17:28:04 +0100

    Update pelicanconf.py

--- pelicanconf.py
+++ pelicanconf.py
@@ -40,7 +40,7 @@ SOCIAL = (('You can add links in your config file', '#'),
           ('Another social link', '#'),
           )
 
-DEFAULT_PAGINATION = 10
+DEFAULT_PAGINATION = 20
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
