125163475641f6d385f549371bb79dcdcd95d557
Matthew C. Kelley <matthew.curtis.kelley@gmail.com>
2022-10-02 21:30:53 -0700

    Correct import statement

---
+++ content/pages/search.md
@@ -0,0 +1,4 @@
+Title: search
+
+Search: <input data-stork="sitesearch" />
+<div data-stork="sitesearch-output"></div>
--- pelicanconf.py
+++ pelicanconf.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*- #
 
-import os, pelican_search
+import os
 
 AUTHOR = 'admin'
 SITENAME = 'SLRB'
